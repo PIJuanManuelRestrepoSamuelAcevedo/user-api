0d9e9ad52375827f4c83c814f15d7ab2d78dd957
manuelRestrep0 <juanrestrepo1221@gmail.com>
2024-09-15 21:35:47 -0500

    add open api config

---
+++ src/main/java/com/udea/proyecto/integrador/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.udea.proyecto.integrador.utils;
+
+public class Constants {
+
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String RESPONSE_ERROR_MESSAGE_KEY = "error";
+    public static final String WRONG_CREDENTIALS_MESSAGE = "Wrong credentials";
+    public static final String SWAGGER_TITLE_MESSAGE = "User API";
+    public static final String SWAGGER_DESCRIPTION_MESSAGE = "User microservice";
+    public static final String SWAGGER_VERSION_MESSAGE = "1.0.0";
+    public static final String SWAGGER_LICENSE_NAME_MESSAGE = "Apache 2.0";
+    public static final String SWAGGER_LICENSE_URL_MESSAGE = "http://springdoc.org";
+    public static final String SWAGGER_TERMS_OF_SERVICE_MESSAGE = "http://swagger.io/terms/";
+}
